6fa28bb519266fa121dd77aa060220a4de6397ed
dsalgos <ds.algos143@gmail.com>
2024-01-23 11:17:42 -0500

    Practicing recursion, GeeksforGeeks course.

---
+++ src/main/java/dsalgos/recursion/SumOfSubsetsToK.java
@@ -0,0 +1,26 @@
+package dsalgos.recursion;
+
+public class SumOfSubsetsToK {
+
+    public  static int result = 0;
+    public static void main(String[] args) {
+        final int arr[] = {8,2,6,4};
+        final int sum = 10;
+        System.out.println(findSubsetsCount(arr, sum, 0));
+    }
+
+    static int findSubsetsCount(int arr[], int sum, int level) {
+        if(level == arr.length) {
+            return sum == 0 ? 1 : 0;
+        }
+
+        int count = 0;
+        if(sum < arr[level])
+            count = findSubsetsCount(arr, sum, level+1);
+
+        count = findSubsetsCount(arr, sum, level+1);
+        count +=findSubsetsCount(arr, sum-arr[level], level+1);
+
+        return count;
+    }
+}
