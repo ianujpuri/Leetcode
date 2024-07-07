7668cd06dc046f24bcedcfe59a1eed20b7b4f6da
puri72uwin <puri72@uwindsor.ca>
2024-07-07 16:56:44 -0400

    G4G Practice questions.

---
+++ src/main/java/dsalgos/arrays/SortedArrayCheck.java
@@ -0,0 +1,17 @@
+package dsalgos.arrays;
+
+public class SortedArrayCheck {
+
+    public boolean isSorted(int[] arr) {
+        for(int i=0; i < arr.length-1; i++) {
+            if (arr[i] > arr[i+1]) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1};
+        SortedArrayCheck check = new SortedArrayCheck();
+        System.out.println(check.isSorted(arr));
+    }
+}
