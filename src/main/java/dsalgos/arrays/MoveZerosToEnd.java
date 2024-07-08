f25b8b81649cfe79d9a8f1e04b7bfb5f46f6aa90
puri72uwin <puri72@uwindsor.ca>
2024-07-08 17:11:31 -0400

    DSA Practice questions.

--- src/main/java/dsalgos/arrays/MoveZerosToEnd.java
+++ src/main/java/dsalgos/arrays/MoveZerosToEnd.java
@@ -1,24 +1,28 @@
 package dsalgos.arrays;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class MoveZerosToEnd {
 
     public static void main(String[] args) {
         int[] arr = {8, 5, 0, 0, 10, 0, 0, 20, 30, 0};
-        arr = moveZeroes(arr);
-        Arrays.stream(arr).forEach((e) -> System.out.print(e + " "));
+        int[] arr1 = moveZeroes(arr);
+        Arrays.stream(arr1).forEach((e) -> System.out.print(e + " "));
+
+        System.out.println();
+
+        int[] arr2 = moveZeroesConcise(arr);
+        Arrays.stream(arr2).forEach((e) -> System.out.print(e + " "));
     }
 
     public static int[] moveZeroes(int[] nums) {
         int counter = 0, zPos = 0;
         while (counter < nums.length) {
             if (nums[zPos] == 0) {
-                while(counter < nums.length && nums[counter] == 0) {
+                while (counter < nums.length && nums[counter] == 0) {
                     counter++;
                 }
-                if(counter < nums.length) {
+                if (counter < nums.length) {
                     nums[zPos++] = nums[counter];
                     nums[counter++] = 0;
                 }
@@ -30,6 +34,16 @@ public class MoveZerosToEnd {
         return nums;
     }
 
-
+    public static int[] moveZeroesConcise(int[] nums) {
+        int counter = 0;
+        for (int pos = 0; pos < nums.length; pos++) {
+            if (nums[pos] != 0) {
+                int temp = nums[counter];
+                nums[counter++] = nums[pos];
+                nums[pos] = temp;
+            }
+        }
+        return nums;
+    }
 
 }
