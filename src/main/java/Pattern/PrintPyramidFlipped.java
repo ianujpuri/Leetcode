efd6819a61fc89f256eb5f9931c8874739653a24
ianujpuri <ds.algos143@gmail.com>
2024-09-15 12:41:22 -0700

    Practice pattersn Striver

--- src/main/java/Pattern/PrintPyramidFlipped.java
+++ src/main/java/Pattern/PrintPyramidFlipped.java
@@ -1,28 +1,21 @@
 package Pattern;
 
-/**
- *          *
- *         ***
- *        *****
- *       *******
- *      *********
- */
-public class PrintStarsAPSeries {
+public class PrintPyramidFlipped {
 
     public static void main(String[] args) {
-        printStars(5);
+        printStars(4);
     }
 
     public static void printStars(int n) {
-        for(int i=1;i<=n;i++) {
-            for(int s=1;s<=n-i;s++) {
+
+        for(int i=n; i>=1; i--) {
+            for(int s=1; s<=n-i; s++) {
                 System.out.print(" ");
             }
-            for(int j=1;j<=(2*(i-1)+1); j++) {
+            for(int j=2*(i-1)+1; j >=1; j--) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
-
 }
---
+++ src/main/java/Pattern/PrintStarsDiamond.java
@@ -0,0 +1,11 @@
+package Pattern;
+
+public class PrintStarsDiamond {
+
+    public static void main(String[] args) {
+        PrintStarsPyramid.printStars(4);
+        PrintPyramidFlipped.printStars(4);
+    }
+
+
+}
