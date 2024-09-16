7d2ed0cc366febcd7bb24da19a47ab659652dd5f
ianujpuri <ds.algos143@gmail.com>
2024-09-15 20:48:56 -0700

    Practice pattersn Striver

---
+++ src/main/java/Pattern/PrintStarsIncThenDec.java
@@ -0,0 +1,10 @@
+package Pattern;
+
+public class PrintStarsIncThenDec {
+
+    public static void main(String[] args) {
+        int n=4;
+        PrintStarsIncreasing.printStars(n);
+        PrintStarsDecreasing.printStars(n-1);
+    }
+}
